732deaabe3817cfa8744bf0c924416f04248c9d7
Louis Bajard <louis.bajard@epita.fr>
2022-11-22 19:12:57 +0100

    Encore plus de composition

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,14 +1,17 @@
 package fr.lernejo.logger;
 
 import java.io.File;
+import java.util.function.Predicate;
 
 public class LoggerFactory {
 
 
     public static Logger getLogger(String name) {
+        Predicate<String> condition = message -> !message.contains("player");
         new File("./log");
         Logger fileLogger = new FileLogger("./log/log");
-        Logger contextualFileLogger = new ContextualLogger(name, fileLogger);
+        Logger filteredFileLogger = new FilteredLogger(fileLogger, condition);
+        Logger contextualFileLogger = new ContextualLogger(name, filteredFileLogger);
         Logger consoleLogger = new ConsoleLogger();
         Logger contextualConsoleLogger = new ContextualLogger(name, consoleLogger);
 
